0a305826822899ecfbfdf135941d31b78caa7233
jay.parmar <jay.p@biztechcs.com>
2022-12-15 19:34:52 +0530

    Link component navigation

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,9 +1,27 @@
-function ProductList() {
+import Link from "next/link"
+
+function ProductList( { productId = 100 } ) {
     return (
         <>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
+            <h2>
+                <Link legacyBehavior href="/"><a>Home</a></Link>
+            </h2>
+
+            <h2>
+                <Link legacyBehavior href="/product/1"><a>Product 1</a></Link>
+            </h2>
+
+            <h2>
+                <Link legacyBehavior href="/product/2"><a>Product 2</a></Link>    
+            </h2>
+
+            <h2>
+                <Link legacyBehavior href="/product/3" replace><a>Product 3</a></Link>
+            </h2>
+
+            <h2>
+                <Link legacyBehavior href={`/product/${productId}`}><a>Product {productId}</a></Link>            
+            </h2>
         </>
     )
 }
