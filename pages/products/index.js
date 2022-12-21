cddfdcc841aa4129fad711bfa3eb2ffd8f64842b
jay.parmar <jay.p@biztechcs.com>
2022-12-21 17:44:34 +0530

    Regenerate data

--- pages/products/index.js
+++ pages/products/index.js
@@ -23,12 +23,15 @@ function ProductList({ products }) {
 export default ProductList
 
 export async function getStaticProps() {
+    console.log('Generating / Regenerating Product List')
+
     const response = await fetch('http://localhost:4000/products')
     const data = await response.json()
 
     return {
         props: {
             products: data,
-        } 
+        },
+        revalidate: 10, 
     }
 }
